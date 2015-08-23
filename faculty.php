4554af5c8e4078e47d262511f9b621bf47d6ce94
Zafarali Ahmed <zafarali.ahmed@gmail.com>
2015-08-23 12:00:27 -0400

    Add circular images to pages, add background color for collapsable navigation bar

--- faculty.php
+++ faculty.php
@@ -3,9 +3,60 @@
 <div class="container-fluid">
 	<section class="row first-section" id="faculty">
 
+	<div class="col-md-8 col-md-offset-2">
+		<div class="person-card">
 
+			<img src="http://aguada.biol.mcgill.ca/photos/head_of_the_thing.png" class="img-circle" style="height:100px; width:100px"> 
+			<h4>JACKIE VOGEL</h4>
+			<p>
+				Head of the Quantitative Biology Program
+			</p>
+		</div>
+		<div class="person-card">
+
+			<img src="http://aguada.biol.mcgill.ca/photos/head_of_the_thing.png" class="img-circle" style="height:100px; width:100px">
+
+			<h4>JACKIE VOGEL</h4>
+			<p>
+				Head of the Quantitative Biology Program
+			</p>
+		</div>
+		<div class="person-card">
+
+			<img src="http://aguada.biol.mcgill.ca/photos/head_of_the_thing.png" class="img-circle" style="height:100px; width:100px">
+			<h4>JACKIE VOGEL</h4>
+			<p>
+				Head of the Quantitative Biology Program
+			</p>
+		</div>
+		<div class="person-card">
+
+			<img src="http://aguada.biol.mcgill.ca/photos/head_of_the_thing.png" class="img-circle" style="height:100px; width:100px">
+			<h4>JACKIE VOGEL</h4>
+			<p>
+				Head of the Quantitative Biology Program
+			</p>
+		</div>
+		<div class="person-card">
+
+			<img src="http://aguada.biol.mcgill.ca/photos/head_of_the_thing.png" class="img-circle" style="height:100px; width:100px">
+			<h4>JACKIE VOGEL</h4>
+			<p>
+				Head of the Quantitative Biology Program
+			</p>
+		</div>
+		<div class="person-card">
+
+			<img src="http://aguada.biol.mcgill.ca/photos/head_of_the_thing.png" class="img-circle" style="height:100px; width:100px">
+			<h4>JACKIE VOGEL</h4>
+			<p>
+				Head of the Quantitative Biology Program
+			</p>
+		</div>
+
+	</div>
 <!-- @TODO: make these into boxes which automatically resize and rearrage as necessary -->
-		<div class="container-fluid">
+		<!-- <div class="container-fluid">
 			<div class="row">
 
 			<div class="col-sm-1 col-sm-offset-3 col-md-1 col-md-offset-3">
@@ -19,7 +70,7 @@
 			<div class="col-sm-1 col-md-1">
 				JACKIE VOGEL
 			</div>
-		</div>
+		</div> -->
 
 	</section>
 
