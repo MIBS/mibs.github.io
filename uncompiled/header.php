bb98e443dde4fe55c538df7306bae0f03d5a32b0
Zafarali Ahmed <zafarali.ahmed@gmail.com>
2015-08-28 20:49:12 -0400

    Update link

--- uncompiled/header.php
+++ uncompiled/header.php
@@ -67,7 +67,7 @@
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-        <li><a href="./index.php#more">About</a></li>
+        <li><a href="./index.html#more">About</a></li>
         <li><a href="./team.php">Team</a></li>
         <li><a href="./faculty.php">Faculty</a></li>
         <li><a href="./links.php">Links</a></li>
