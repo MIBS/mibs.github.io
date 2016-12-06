d411b2d4acb8202536a2359c9cad28dc13f7cd9a
Zafarali Ahmed <zafarali.ahmed@gmail.com>
2016-12-06 14:35:16 -0500

    Add pictures and placeholders for team page, close #3

--- uncompiled/team.php
+++ uncompiled/team.php
@@ -9,106 +9,107 @@
 			Team
 		</h2>
 		<hr />
-<!-- 	COMMENTED OUT FOR NOW	
+
 		<div class="person-card">
-			<img src="mibs.github.io/images/pictures/Kevin.jpg" class="img-circle" style="height:100px; width:100px"> 
-			<h4>Kevin Luk</h4>
+			<img src="./images/pictures/Hannah.jpg" class="img-circle" style="height:100px; width:100px"> 
+			<h4>Hannah Burr</h4>
 			<p>
-				Founder and President
+				President
 			</p>
 		</div>
 		<div class="person-card">
-			<img src="mibs.github.io/images/pictures/Anna.jpg" class="img-circle" style="height:100px; width:100px">
-			<h4>Anna Abraham</h4>
+			<img src="./images/pictures/Athy.jpg" class="img-circle" style="height:100px; width:100px">
+			<h4>Athy Ambikkumar</h4>
 			<p>
-				VP Administration and Communications
+				VP Finance
 			</p>
 		</div>
+                <div class="person-card">
+                        <img src="./images/pictures/Anna.jpg" class="img-circle" style="height:100px; width:100px">
+                        <h4>Anna Abraham</h4>
+                        <p>
+                                VP Communications
+                        </p>
+                </div>
+                <div class="person-card">
+                        <img src="https://thebenclark.files.wordpress.com/2014/03/facebook-default-no-profile-pic.jpg" class="img-circle" class="img-circle" style="height:100px; width:100px">
+                        <h4>Simran Rahi</h4>
+                        <p>
+                                VP Events
+                        </p>
+                </div>
 		<div class="person-card">
-			<img src="mibs.github.io/images/pictures/Ariel.jpg" class="img-circle" style="height:100px; width:100px">
+			<img src="./images/pictures/Ariel.jpg" class="img-circle" style="height:100px; width:100px">
 			<h4>Ariel Greiner</h4>
 			<p>
 				VP Events
 			</p>
 		</div>
 		<div class="person-card">
-                        <img src="mibs.github.io/images/pictures/Zaf.jpg" class="img-circle" style="height:100px; width:100px">
+                        <img src="./images/pictures/Zaf.jpg" class="img-circle" style="height:100px; width:100px">
                         <h4>Zafarali Ahmed</h4>
                         <p>
                                 VP Events
                         </p>
                 </div>
 		<div class="person-card">                        
-		<img src="mibs.github.io/images/pictures/Athy.jpg" class="img-circle" style="height:100px; width:100px">
-                        <h4>Athy Ambikkumar</h4>
+		<img src="https://thebenclark.files.wordpress.com/2014/03/facebook-default-no-profile-pic.jpg" class="img-circle" style="height:100px; width:100px">
+                        <h4>Zoe Hu</h4>
+                        <p>
+                                VP Fundraising
+                        </p>
+                </div>
+                <div class="person-card">                        
+                <img src="https://thebenclark.files.wordpress.com/2014/03/facebook-default-no-profile-pic.jpg" class="img-circle" class="img-circle" style="height:100px; width:100px">
+                        <h4>Jason Yao</h4>
                         <p>
                                 VP Fundraising
                         </p>
                 </div>
 		<div class="person-card">                        
-                <img src="mibs.github.io/images/pictures/Bianca.jpg" class="img-circle" style="height:100px; width:100px">
-                        <h4>Bianca Brusco</h4>
+                <img src="./images/pictures/Jia.jpeg" class="img-circle" style="height:100px; width:100px">
+                        <h4>Jia Lu</h4>
                         <p>
                                 Joint Mathematics and Biology Representative
                         </p>
                 </div>
 		<div class="person-card">
-                <img src="mibs.github.io/images/pictures/Cassandra.jpg" class="img-circle" style="height:100px; width:100px">
-                        <h4>Cassandra Rogers</h4>
+                <img src="https://thebenclark.files.wordpress.com/2014/03/facebook-default-no-profile-pic.jpg" class="img-circle" class="img-circle" style="height:100px; width:100px">
+                        <h4>Selin Jessa</h4>
                         <p>
                                 Joint Computer Science and Biology Representative
                         </p>
                 </div>
 		<div class="person-card">
-                <img src="mibs.github.io/images/pictures/Cassandra.jpg" class="img-circle" style="height:100px; width:100px">
-                        <h4>Cassandra Rogers</h4>
-                        <p>
-                                Joint Computer Science and Biology Representative
-                        </p>
-                </div>
-		<div class="person-card">
-                <img src="mibs.github.io/images/pictures/Ronnie.jpg" class="img-circle" style="height:100px; width:100px">
-                        <h4>Ronnie Davies</h4>
-                        <p>
-                                Joint Computer Science and Biology Representative
-                        </p>
-                </div>
-		<div class="person-card">
-                <img src="mibs.github.io/images/pictures/Hannah.jpg" class="img-circle" style="height:100px; width:100px">
-                        <h4>Hannah Burr</h4>
-                        <p>
-                        	VP Finances
-			</p>
-                </div>
-		<div class="person-card">
-                <img src="mibs.github.io/images/pictures/Katie.jpg" class="img-circle" style="height:100px; width:100px">
-                        <h4>Katie Hindson</h4>
-                        <p>
-                        	Quantitative Biology Representative
-			</p>    
-                </div>
-		<div class="person-card">
-                <img src="mibs.github.io/images/pictures/Malina.jpg" class="img-circle" style="height:100px; width:100px">
+                <img src="./images/pictures/Malina.jpg" class="img-circle" style="height:100px; width:100px">
                         <h4>Malina Iwanski</h4>
                         <p>
                                 Quantitative Biology Representative
                         </p>
                 </div>
 		<div class="person-card">
-                <img src="mibs.github.io/images/pictures/Richard.jpg" class="img-circle" style="height:100px; width:100px">
-                        <h4>Richard Zhang</h4>
+                <img src="https://thebenclark.files.wordpress.com/2014/03/facebook-default-no-profile-pic.jpg" class="img-circle" class="img-circle" style="height:100px; width:100px">
+                        <h4>Polina Poppel</h4>
                         <p>
-                        	VP Fundraising
+                        	Physiology and Mathematics Representative
 			</p>
                 </div>
-		<div class="person-card">
-                <img src="mibs.github.io/images/pictures/Zsombar.jpg" class="img-circle" style="height:100px; width:100px">
-                        <h4>Zsombor Balassy</h4>
+                <div class="person-card">
+                <img src="https://thebenclark.files.wordpress.com/2014/03/facebook-default-no-profile-pic.jpg" class="img-circle" style="height:100px; width:100px">
+                        <h4>Alexander Friesen</h4>
+                        <p>
+                                Physics and Physiology Representative
+                        </p>
+                </div>
+                <div class="person-card">
+                <img src="https://thebenclark.files.wordpress.com/2014/03/facebook-default-no-profile-pic.jpg" class="img-circle" class="img-circle" style="height:100px; width:100px">
+                        <h4>Katelyn Bouffard</h4>
                         <p>
                         	Physics and Physiology Representative
 			</p>
                 </div>
- -->		<div class="person-card">
+		
+                <div class="person-card">
 
 			<img src="https://thebenclark.files.wordpress.com/2014/03/facebook-default-no-profile-pic.jpg" class="img-circle" style="height:100px; width:100px">
 			<h4>You</h4>
